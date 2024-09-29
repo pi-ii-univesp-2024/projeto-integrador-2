3f36dda788ce57a173e95c3e1c6dbacb5d042940
Jobe Moraes <jobecamera@gmail.com>
2024-09-29 18:59:36 -0300

    fix: loader no local correto em categorias-produto

--- frontend/src/pages/categorias-produto/index.js
+++ frontend/src/pages/categorias-produto/index.js
@@ -5,24 +5,25 @@ import { Box, CircularProgress, Stack, Typography } from "@mui/material";
 export default function CategoriasProduto() {
   const { data: categoriasProduto, isLoading } = useCategoriasProduto();
 
-  if (isLoading) return <CircularProgress />;
-
   return (
     <MainLayout>
-      <Box>
-        <Typography variant="h1">Categorias produto</Typography>
-        <Stack gap={1} paddingTop={2}>
-          {categoriasProduto?.map((categoria) => (
-            <Typography
-              key={categoria.id}
-              variant="body1"
-              title={categoria.nome}
-            >
-              {categoria.id} - {categoria.nome}
-            </Typography>
-          ))}
-        </Stack>
-      </Box>
+      {isLoading && <CircularProgress />}
+      {!isLoading && (
+        <Box>
+          <Typography variant="h1">Categorias produto</Typography>
+          <Stack gap={1} paddingTop={2}>
+            {categoriasProduto?.map((categoria) => (
+              <Typography
+                key={categoria.id}
+                variant="body1"
+                title={categoria.nome}
+              >
+                {categoria.id} - {categoria.nome}
+              </Typography>
+            ))}
+          </Stack>
+        </Box>
+      )}
     </MainLayout>
   );
 }
