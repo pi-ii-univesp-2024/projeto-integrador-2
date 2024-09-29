4ae005c90bd70d67671e0e50c73e4d9ef7c06986
Jobe Moraes <jobecamera@gmail.com>
2024-09-29 18:53:23 -0300

    fix: local de queryClient.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -2,9 +2,9 @@ import "@/styles/globals.css";
 import theme from "@/config/theme";
 import { ThemeProvider } from "@mui/material";
 import { QueryClientProvider } from "@tanstack/react-query";
-import queryClient from "@/queryClient";
 import Head from "next/head";
 import { SidebarProvider } from "@/contexts/SidebarContext";
+import queryClient from "@/config/queryClient";
 
 export default function App({ Component, pageProps }) {
   return (
