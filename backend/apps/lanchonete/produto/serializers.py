bc20b16e7743b04ed0df6ed0891343df6aadaaf1
Jobe Moraes <jobecamera@gmail.com>
2024-10-20 11:17:41 -0300

    fix: registro de estoque pela api

--- backend/apps/lanchonete/produto/serializers.py
+++ backend/apps/lanchonete/produto/serializers.py
@@ -16,8 +16,25 @@ class ProdutoSerializer(serializers.ModelSerializer):
         return produto_instance
 
     def update(self, instance, validated_data):
-        # Atualização do produto com o comportamento padrão do modelo
+        # Guarda a quantidade de estoque antiga
+        quantidade_estoque_antiga = instance.quantidade_estoque
+
+        # Atualiza os atributos do produto
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
+
         instance.save()
+
+        # Verifica se a quantidade de estoque foi alterada
+        if 'quantidade_estoque' in validated_data:
+            if quantidade_estoque_antiga != instance.quantidade_estoque:
+                # Se a quantidade foi alterada, registra a movimentação de estoque
+                diferenca_estoque = instance.quantidade_estoque - quantidade_estoque_antiga
+                tipo = 'ENTRADA' if diferenca_estoque > 0 else 'SAIDA'
+                instance.criar_movimentacao_estoque(
+                    quantidade=abs(diferenca_estoque),
+                    tipo=tipo,
+                    observacao='Produto atualizado com ajuste de estoque.'
+                )
+
         return instance
